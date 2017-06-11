a0f4f6c709c85f23552887178e2b87d25cf9612c
MuffinTheMan <larsenc91@gmail.com>
2017-06-10 22:24:21 -0600

    Initial commit.

---
+++ classes.rb
@@ -0,0 +1,42 @@
+class Ruby
+  SPACER = "# ----- ----- ----- ------ ----- ----- ----- #"
+  AUTHOR_HEADER = "# Written by MuffinTheMan (aka Caleb Larsen) #"
+  MONKEYPATCHING_HEADER = "#               MonkeyPatching               #"
+  SCRIPT_HEADER = "#                   Script                   #"
+  COMMENT_MARKER = "#"
+  OPEN_FILE = "# Open file (name supplied as first argument) and read through one line at a time.\n\
+File.open(ARGV[0]).each_line do |line|"
+end
+
+class PHP
+  SPACER = "// ----- ----- ----- ------ ----- ----- ----- //"
+  AUTHOR_HEADER = "// Written by MuffinTheMan (aka Caleb Larsen) //"
+  MONKEYPATCHING_HEADER = nil
+  SCRIPT_HEADER = "//                   Script                   //"
+  COMMENT_MARKER = "//"
+  OPEN_FILE = "// Open file (name supplied as first argument) and read through one line at a time.\n\
+  $fh = fopen($argv[1], \"r\");\n\n\
+  while ($line = fgets($fh)) {\n\
+  }"
+end
+
+class Java
+  SPACER = "// ----- ----- ----- ------ ----- ----- ----- //"
+  AUTHOR_HEADER = "// Written by MuffinTheMan (aka Caleb Larsen) //"
+  MONKEYPATCHING_HEADER = nil
+  SCRIPT_HEADER = "//                   Script                   //"
+  COMMENT_MARKER = "//"
+  OPEN_FILE = "import java.io.*;\n\n\
+  public class Main {\n\
+      public static void main (String[] args) throws IOException {\n\
+          // Open file (name supplied as first argument) and read through one line at a time.\n\
+          File file = new File(args[0]);\n\
+          BufferedReader buffer = new BufferedReader(new FileReader(file));\n\
+          String line;\n\
+          while ((line = buffer.readLine()) != null) {\n\
+              line = line.trim();\n\
+              // Process line of input Here\n\
+          }\n\
+      }\n\
+  }"
+end
---
+++ generate.rb
@@ -0,0 +1,74 @@
+# ----- ----- ----- ------ ----- ----- ----- #
+# Written by MuffinTheMan (aka Caleb Larsen) #
+# ----- ----- ----- ------ ----- ----- ----- #
+#
+# Program written in order to generate skeleton programs for CodeEval cahallenges.
+#
+require_relative "classes"
+#
+# ----- ----- ----- ------ ----- ----- ----- #
+#                 Variables                  #
+# ----- ----- ----- ------ ----- ----- ----- #
+#
+difficulties = ["easy","moderate","hard"]
+languages = ["Ruby","PHP","Java"]
+file_endings = Hash["Ruby" => "rb", "PHP" => "php", "Java" => "java"]
+#
+# ----- ----- ----- ------ ----- ----- ----- #
+#                   Script                   #
+# ----- ----- ----- ------ ----- ----- ----- #
+#
+print "What is the difficulty of the challenge? (easy/moderate/hard) "
+difficulty = gets.chomp
+unless difficulties.include?(difficulty)
+  puts "Next time, try 'easy' or 'moderate' or 'hard'"
+  exit
+end
+
+print "What language are you using? (Ruby/PHP/Java) "
+language = gets.chomp
+unless languages.include?(language)
+  puts "Next time, try 'Ruby' or 'PHP' or 'Java'"
+  exit
+end
+klass = Object.const_get(language)
+f_ending = file_endings[language]
+
+print "What is the name of the challenge (e.g. String Permutations)? "
+challenge_name = gets.chomp
+unless language === "Java"
+  program_name = challenge_name.downcase.split.join('_') << ".#{f_ending}"
+else
+  program_name = "Main.java"
+end
+directory = "#{difficulty}/incomplete/#{challenge_name.downcase.split.join('_')}-#{f_ending}"
+
+`mkdir #{directory}`
+`touch #{directory}/input.txt`
+`cp makefile-java #{directory}/makefile` if language === "Java"
+
+File.open("#{directory}/#{program_name}",'w') do |f|
+  puts "<?php" if language === "PHP"
+  f.puts klass::SPACER
+  f.puts klass::AUTHOR_HEADER
+  f.puts klass::SPACER
+  f.puts klass::COMMENT_MARKER
+  f.puts "#{klass::COMMENT_MARKER} Program written as solution to \"#{challenge_name}\" challenge on codeeval.com"
+  f.puts "#{klass::COMMENT_MARKER} - Description"
+  f.puts klass::COMMENT_MARKER
+  f.puts klass::SPACER
+  if klass::MONKEYPATCHING_HEADER
+    f.puts klass::MONKEYPATCHING_HEADER
+    f.puts klass::SPACER
+    f.puts klass::COMMENT_MARKER
+    f.puts "class Class"
+    f.puts "end"
+    f.puts klass::COMMENT_MARKER
+    f.puts klass::SPACER
+  end
+  f.puts klass::SCRIPT_HEADER
+  f.puts klass::SPACER
+  f.puts klass::COMMENT_MARKER
+  f.puts klass::OPEN_FILE
+  f.puts "end" if language === "Ruby"
+end
